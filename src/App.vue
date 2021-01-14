349da2d50ca7b485d8f40ee0ac725607145537bd
Stanley Houcke <stanhoucke@gmail.com>
2021-01-14 14:59:02 +0000

    chart showing on vue item click

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template lang="html">
   <div>
     <h1>Our Cool Energy Mix Tracker</h1>
-    <generation-mix :energies-list="energies.generationmix"></generation-mix>
+    <generation-mix :energieslist="energies.generationmix"></generation-mix>
   </div>
 </template>
 
 <script>
-import GenerationMix from './components/GenerationMix.vue'
+// import GenerationMix from './components/GenerationMix.vue'
+import GenerationMixer from './components/GenerationMixer.vue'
 
 export default {
   name: 'app',
   data(){
     return {
-      energies: []
+      energies: {}
     }
   },
   components: {
-    "generation-mix": GenerationMix
+    "generation-mix": GenerationMixer
   },
   mounted(){
     fetch('https://api.carbonintensity.org.uk/generation')
